81f1ae12b31fe4c46b3c5b04dde4e04540baf02f
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2020-01-13 14:25:56 +0100

    Add ApiService

---
+++ app/src/main/java/julien/s/mareu/controller/MeetingApiService.java
@@ -0,0 +1,14 @@
+package julien.s.mareu.controller;
+
+import java.util.ArrayList;
+
+import julien.s.mareu.model.Meeting;
+
+public interface MeetingApiService {
+
+    ArrayList<Meeting> getMeetingsList();
+
+    void addMeeting(Meeting meeting);
+
+    void deleteMeeting(Meeting meeting);
+}
