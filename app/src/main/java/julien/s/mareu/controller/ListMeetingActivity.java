f00aae9bf0f2a6575b4401456360be079b8be2c6
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2020-01-13 12:19:46 +0100

    Revert "Add recyclerview"
    This reverts commit 21eb7b804d5420435b09e727caa06292156a6478.

--- app/src/main/java/julien/s/mareu/controller/ListMeetingActivity.java
+++ app/src/main/java/julien/s/mareu/controller/ListMeetingActivity.java
@@ -1,34 +1,20 @@
 package julien.s.mareu.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-
-import java.util.ArrayList;
+import android.widget.Adapter;
+import android.widget.Toolbar;
 
 import julien.s.mareu.R;
-import julien.s.mareu.model.Meeting;
 
 public class ListMeetingActivity extends AppCompatActivity {
-
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_meeting);
-
-        ArrayList<String> participants = new ArrayList<>();
-        participants.add("Participant");
-
-        ArrayList<Meeting> meetingsList = new ArrayList<>();
-        meetingsList.add(new Meeting("ExempleRoom","Exemple Hour","Exemple Subject",participants));
-        meetingsList.add(new Meeting("ExempleRoom2","Exemple Hour2","Exemple Subject2",participants));
-        meetingsList.add(new Meeting("ExempleRoom3","Exemple Hour3","Exemple Subject3",participants));
 
     }
 }
